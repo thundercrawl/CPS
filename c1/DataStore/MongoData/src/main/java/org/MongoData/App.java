528449ebdeea3600197457bf81ec601eaa7c9fb3
thundercrawl <thundercrawl@IBM223-PBM1ZTA.cn.ibm.com>
2016-10-20 19:56:56 +0800

    Initial Modules

---
+++ c1/DataStore/MongoData/src/main/java/org/MongoData/App.java
@@ -0,0 +1,13 @@
+package org.MongoData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
