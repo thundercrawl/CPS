0509e48a4633fae6533fec8ec68a8c328fef587b
thundercrawl <brightpegion@gmail.com>
2016-10-24 10:44:45 +0800

    System level Config service

---
+++ c1/SystemModule/ConfigService/src/main/java/org/ConfigService/SystemConfig.java
@@ -0,0 +1,71 @@
+package org.ConfigService;
+
+
+
+public enum SystemConfig {
+	CPS_MONITOR_ENABLED(Type.DEFAULT,0)
+
+	public enum Type{
+		DEFAULT,
+		Log
+
+	}
+	private SystemConfig.Type DefaultType;
+	private int DefaultValue;
+	SystemConfig(final Type type,final int value)
+	{
+		DefaultType = type;
+		DefaultValue = value;
+	}
+	public	enum LogLevel{
+		SEVERE(1), WARNING(2), INFO(3), FINE(4), FINER(5), FINEST(6);
+
+		private int level;
+
+		LogLevel(final int level)
+		{
+			this.level = level;
+		}
+
+		public int getLevel()
+		{
+			return level;
+		}
+
+		public static LogLevel valueByInt(final int level)
+		{
+			for (final LogLevel log : values())
+			{
+				if (level == log.level)
+				{
+					return log;
+				}
+			}
+			throw new IllegalArgumentException(level + " is not a recognized value.");
+		}
+
+		public static LogLevel valueByString(final String name)
+		{
+			if (name.length() == 1)
+			{
+				// try by converting string to level
+				try
+				{
+					final int level = Integer.valueOf(name);
+					final LogLevel log = valueByInt(level);
+					if (log != null)
+					{
+						return log;
+					}
+				}
+				catch (final NumberFormatException nfe)
+				{
+					// ignore
+				}
+			}
+			// if we get here, try by name
+			return valueOf(name);
+		}
+	}
+
+}
